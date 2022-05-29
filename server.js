d9f16b2cc6d65488f72b2a2ff4611e97ee49a898
eddybruv <edwinajong@gmail.com>
2022-05-29 05:16:48 +0100

    express server

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const path = require('path');
 
-app.listen(8080, () => console.log("Running stuff"));
+const app = express();
+const router = express.Router();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+const PORT = process.env.PORT || 8080
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
