955aa717c51576c7880f7ec19963c9a72a8979bc
eddybruv <edwinajong@gmail.com>
2022-05-29 09:07:56 +0100

    added routes

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
+
+// routes
+const UserRouter = require("./routes/User.router");
+const BookRouter = require("./routes/Book.router");
 
 const app = express();
-const router = express.Router();
+const dbConnnection = mongoose.connect(process.env.MONGODB_URL);
+
+dbConnnection.then(() => console.log("DB Connected...")).catch(() => console.log("DB not connected..."))
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
@@ -13,7 +19,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-const PORT = process.env.PORT || 8080
+app.use("/api/user", UserRouter);
+app.use("/api/book", BookRouter);
+
+const PORT = process.env.PORT || 8080;
 
 app.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
