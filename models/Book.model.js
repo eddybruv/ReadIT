5d76bbb7841a1e68339269f2f975bb94a94159b9
eddybruv <edwinajong@gmail.com>
2022-05-29 05:17:19 +0100

    created book model

---
+++ models/Book.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishedDate: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    pageCount: { type: Number, required: true },
+    smallThumbnail: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    previewLink: {
+      type: String,
+      required: true,
+    },
+    saleability: {
+      type: String,
+      required: true,
+    },
+    userID: { type: mongoose.ObjectId, ref: "users" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("books", BookSchema);
