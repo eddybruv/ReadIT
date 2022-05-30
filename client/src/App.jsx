0d57b8f55024c3944885758aefc4f2406346239a
eddybruv <edwinajong@gmail.com>
2022-05-30 20:36:50 +0100

    rendered signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css';
-import Login from './components/Login';
+import "./App.css";
+import Signup from './pages/Signup'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: "#e0a101",
+      light: "#debb60",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Signup></Signup>
+      </div>
+    </ThemeProvider>
   );
 }
 
