bd3e871979ffc08a1d307348151a45b66a75b0bc
eddybruv <edwinajong@gmail.com>
2022-06-01 05:31:04 +0100

    added icons and icon color

--- client/src/components/preferencePage/PickPreference.jsx
+++ client/src/components/preferencePage/PickPreference.jsx
@@ -2,35 +2,34 @@ import React from "react";
 import Card from "./Card";
 
 import { Button, Typography } from "@mui/material";
-import SendIcon from '@mui/icons-material/Send';
+import SendIcon from "@mui/icons-material/Send";
 
 import style from "../../styles/preferences.module.css";
 
-
 function PickPreference() {
-  let count = 0
+  let count = 0;
 
   const preferences = [
-    {value: "Romance"},
-    {value: "Horror"},
-    {value: "Survival"},
-    {value: "Adventure"},
-    {value: "Society"},
-    {value: "War"},
-    {value: "Action"},
-    {value: "Detective"},
-    {value: "Fantasy"},
-    {value: "Fiction"},
-    {value: "Poetry"},
-    {value: "Science"},
-    {value: "Thriller"},
-    {value: "History"},
-    {value: "Technology"},
-    {value: "Crime"},
-    {value: "Programming"},
-    {value: "Children's"},
-    {value: "Drama"},
-    {value: "Mystery"}
+    { value: "Romance" },
+    { value: "Horror" },
+    { value: "Survival" },
+    { value: "Adventure" },
+    { value: "Society" },
+    { value: "War" },
+    { value: "Action" },
+    { value: "Detective" },
+    { value: "Fantasy" },
+    { value: "Fiction" },
+    { value: "Poetry" },
+    { value: "Science" },
+    { value: "Thriller" },
+    { value: "History" },
+    { value: "Technology" },
+    { value: "Crime" },
+    { value: "Programming" },
+    { value: "Children's" },
+    { value: "Drama" },
+    { value: "Mystery" },
   ];
 
   return (
@@ -53,7 +52,7 @@ function PickPreference() {
         <Button
           color="secondary"
           variant="contained"
-          endIcon={<SendIcon  />}
+          endIcon={<SendIcon style={{ fill: "#1A2D31" }} />}
         >
           Get Started
         </Button>
