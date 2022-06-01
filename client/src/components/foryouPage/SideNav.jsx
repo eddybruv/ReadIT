bf3b7a962ea8f8e791b76470d16280ae79f7077b
eddybruv <edwinajong@gmail.com>
2022-06-01 22:18:50 +0100

    added icons

--- client/src/components/foryouPage/SideNav.jsx
+++ client/src/components/foryouPage/SideNav.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 // import {Link, useNavigate} from 'react-router-dom';
 
 import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import LocalLibraryRoundedIcon from "@mui/icons-material/LocalLibraryRounded";
+import IconButton from "@mui/material/IconButton";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import GroupsIcon from "@mui/icons-material/Groups";
+import  Tooltip  from '@mui/material/Tooltip';
 
 import style from "../../styles/sideNav.module.css";
 import logo from '../../images/logo.png'
@@ -14,10 +20,37 @@ function SideNav() {
         <img src={logo} className="img-fluid" alt="" />
       </div>
       <div className={`${style.icons}`}>
-        <LibraryBooksIcon fontSize='large'/>
+        <Tooltip title="Home Page">
+          <IconButton>
+            <LibraryBooksIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="My Books">
+          <IconButton>
+            <LocalLibraryRoundedIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="Book Club">
+          <IconButton>
+            <GroupsIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+
+        <Tooltip title="Settings">
+          <IconButton>
+            <SettingsOutlinedIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+      </div>
+
+      <div className={`${style.logOut} ${style.icons}`}>
+        <IconButton>
+          <LogoutIcon fontSize="large" />
+        </IconButton>
       </div>
     </section>
-  )
+  );
 }
 
 export default SideNav
