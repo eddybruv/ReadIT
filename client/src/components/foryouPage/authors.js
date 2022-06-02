07723a82df90605d126f9ad771aae3d487247925
eddybruv <edwinajong@gmail.com>
2022-06-02 22:17:32 +0100

    array of authors and info

---
+++ client/src/components/foryouPage/authors.js
@@ -0,0 +1,67 @@
+const authors = [
+  {
+    name: "George Orwell",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/8/82/George_Orwell%2C_c._1940_%2841928180381%29.jpg",
+    value: "george orwell",
+    num: 6,
+    occ: "Writer, Literary critic"
+  },
+  {
+    name: "Stephen King",
+    imgUrl:
+      "https://i0.wp.com/www.heyuguys.com/images/2011/08/Stephen-King.jpg?w=2088&ssl=1",
+    value: "stephen king",
+    num: 63
+  },
+  {
+    name: "Stan Lee",
+    imgUrl:
+      "https://gagadget.com/media/cache/9c/79/9c7945026f4f835ad2c2f6a64f219b56.jpg",
+    value: "stan lee",
+    num: 217,
+    occ: "Film writter"
+  },
+  {
+    name: "William Shakespeare",
+    imgUrl:
+      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE1ODA0OTcxNzgzMzkwNzMz/william-shakespeare-194895-1-402.jpg",
+    value: "william shakespeare",
+    num: 202,
+    occ: "Poet, Playwrite"
+  },
+  {
+    name: "Chimamanda N.",
+    imgUrl:
+      "https://lelab.info/wp-content/uploads/2021/09/60F1D66E-FB64-4D8B-870E-717322FA625A.png",
+    value: "chimamanda ngozi adiche",
+    num: 6,
+    occ: "Author Teacher"
+  },
+  {
+    name: "Chima Achebe",
+    imgUrl:
+      "https://www.gannett-cdn.com/media/USATODAY/USATODAY/2013/03/22/ap-obit-achebe-3_4.jpg?width=300&height=402&fit=crop&format=pjpg&auto=webp",
+    value: "chimia achebe",
+    num: 3,
+    occ: "Philosopher, Essayist"
+  },
+  {
+    name: "Wole Soyinka",
+    imgUrl:
+      "https://www.financialafrik.com/wp-content/uploads/2020/05/Wole-Soyinka-696x497.jpg",
+    value: "wole soyinka",
+    num: 52,
+    occ: "Translator, Poet"
+  },
+  {
+    name: "Charles Dickens",
+    imgUrl:
+      "https://i.guim.co.uk/img/media/c4c3ed5b82928811b804cc817f77da2e15ea228b/0_372_2400_1440/master/2400.jpg?width=1020&quality=85&auto=format&fit=max&s=cbddfc5a9b8e479b8d8a7d025cdb954c",
+    value: "charles dickens",
+    num: 15,
+    occ: "Playwrite, Poet"
+  },
+];
+
+export default authors
