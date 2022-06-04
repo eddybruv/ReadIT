aa1e4e03cb8cadf322d3c3dff6251459373941af
eddybruv <edwinajong@gmail.com>
2022-06-04 11:49:31 +0100

    removed required fields

--- models/User.model.js
+++ models/User.model.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 // create user model
 const UserSchema = new mongoose.Schema(
   {
-    name: {type: String, required: true },
-    email: {type: String, required: true  },
-    password: {type: String, required: true },
-    username: {type: String, required: true },
-    imageUrl: {type: String, required: true },
-    preference: {type: [String]}
+    name: {type: String },
+    email: {type: String  },
+    password: {type: String },
+    username: {type: String },
+    imageUrl: {type: String },
+    preference: {type: []}
   },
   { timestamps: true }
 );
