596eb42122183800c940c3371afe39d7b74272c5
eddybruv <edwinajong@gmail.com>
2022-06-04 11:49:03 +0100

    removed required

--- routes/User.router.js
+++ routes/User.router.js
@@ -11,18 +11,38 @@ router.post("/create-user", (req, res) => {
     email,
     password,
     username,
-    imageUrl,
+    imageUrl: '',
     preference,
   });
   newUser.save();
   res.json({ message: "new user created", data: newUser });
 });
 
-router.get("/get-users", (req, res) => {
-  const users = UserModel.find({});
+router.get("/get-users", async (req, res) => {
+  const users = await UserModel.find({});
   users
     ? res.json({ message: "found users", data: users })
     : res.json({ message: "no user found" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email, password });
+
+  user
+    ? res.json({ message: "found user", data: user })
+    : res.json({ message: "user not found" });
+});
+
+router.post("/update-user", async (req, res) => {
+  const { _id, imageUrl, preference } = req.body;
+  const user = await UserModel.findByIdAndUpdate(_id, { imageUrl, preference });
+  
+  user.save();
+
+  user
+    ? res.json({ message: "user updated", data: user })
+    : res.json({ message: "user not updated" });
 });
 
 module.exports = router;
