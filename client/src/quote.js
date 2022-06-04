2127287fffbda3113e7f2bc5d07aeb643f421d34
eddybruv <edwinajong@gmail.com>
2022-06-04 12:01:29 +0100

    quotes

---
+++ client/src/quote.js
@@ -0,0 +1,15 @@
+const quotes = [
+  '"Many people, myself among them, feel better at the mere sight of a book."',
+  '"The library is inhabited by spirits that come out of the pages at night."',
+  "\"If you don't like to read, you haven't found the right book.\"",
+  '"When I have a little money, I buy books; and if I have any left, I buy food and clothes."',
+  '"Fill your house with stacks of books, in all the crannies and all the nooks."',
+  '"Rainy days should be spent at home with a cup of tea and a good book."',
+  '"Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten."',
+  '"Books are good company, in sad times and happy times, for books are people - people who have managed to stay alive by hiding between the covers of a book."',
+  '"A book is a garden, an orchard, a storehouse, a party, a company by the way, a counselor, a multitude of counselors."',
+];
+
+const quote = quotes[Math.floor(Math.random() * 9)];
+
+export default quote
