90e5904da1aad939d9914c2391b90ac461db14ac
eddybruv <edwinajong@gmail.com>
2022-06-23 19:33:32 +0100

    added a context provider

--- client/src/pages/Preferences.jsx
+++ client/src/pages/Preferences.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Container } from '@mui/material'
 import SelectPhoto from '../components/preferencePage/SelectPhoto';
 import PickPreference from '../components/preferencePage/PickPreference';
+import PreferenceProvider from "../components/preferencePage/PreferenceContext";
 import style from '../styles/preferences.module.css';
 
 function Preferences() {
   return (
+    <PreferenceProvider>
     <section className={`${style.body}`}>
       <Container sx={{ height: "100%" }}>
         <section className={`${style.main}`}>
@@ -14,6 +16,7 @@ function Preferences() {
         </section>
       </Container>
     </section>
+    </PreferenceProvider>
   );
 }
 
