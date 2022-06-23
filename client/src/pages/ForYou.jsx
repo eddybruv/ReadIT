e929225b00940fc2afff7ef82f6377663d9d4384
eddybruv <edwinajong@gmail.com>
2022-06-23 19:33:50 +0100

    added context provider

--- client/src/pages/ForYou.jsx
+++ client/src/pages/ForYou.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PreferenceProvider from "../components/preferencePage/PreferenceContext"
 import style from "../styles/foryou.module.css";
 import SideNav from "../components/foryouPage/SideNav";
 import Hero from "../components/foryouPage/Hero";
@@ -6,6 +7,7 @@ import FeaturedAuthors from "../components/foryouPage/FeaturedAuthors";
 
 function ForYou() {
   return (
+    <PreferenceProvider>
     <section className={`${style.body} container-fluid p-0`}>
       <section className={`${style.sideNav}`}>
         <SideNav />
@@ -17,6 +19,7 @@ function ForYou() {
         <FeaturedAuthors />
       </section>
     </section>
+    </PreferenceProvider>
   );
 }
 
