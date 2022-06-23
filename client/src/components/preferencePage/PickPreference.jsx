269373d1689a3e5fc02362b586d020d46b9b9cae
eddybruv <edwinajong@gmail.com>
2022-06-23 19:35:31 +0100

    modified

--- client/src/components/preferencePage/PickPreference.jsx
+++ client/src/components/preferencePage/PickPreference.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
+import {PreferenceContext} from "./PreferenceContext";
 import Card from "./Card";
 import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 
 import { Button, Typography } from "@mui/material";
@@ -9,10 +11,15 @@ import SendIcon from "@mui/icons-material/Send";
 import style from "../../styles/preferences.module.css";
 
 function PickPreference() {
+  const {preferences, setPreferences} = useContext(PreferenceContext)
   const navigate = useNavigate();
   let user = JSON.parse(sessionStorage.getItem("loggedUser"));
 
-  const preferences = [
+  useEffect(() => {
+    console.log(preferences);
+  }, [preferences])
+
+  const choices = [
     { value: "Romance" },
     { value: "Horror" },
     { value: "Survival" },
@@ -37,48 +44,48 @@ function PickPreference() {
 
   const handleClick = async (e) => {
     e.preventDefault();
-    user.preference = user.preference.flat();
-    console.log(user)
-    sessionStorage.setItem("loggedUser", JSON.stringify(user));
 
-    /* let res = await axios
+     let res = await axios
       .post("/api/user/update-user", {
         _id: user._id,
         imageUrl: user.imageUrl,
-        preference: user.preference,
+        preference: preferences,
       })
       .then((data) => data.data);
-    console.log(res); */
+    sessionStorage.setItem("loggedUser", JSON.stringify(res.data));
     navigate(`/${user.username}`);
-  };;
+  };
 
   return (
-    <div className={`${style.preferencesSection}`}>
-      <Typography
-        sx={{ fontSize: "1.9rem", fontFamily: "Montserrat" }}
-        varient="h2"
-        component="h2"
-      >
-        Select Preferences
-      </Typography>
 
-      <div className={style.preferences}>
-        {preferences.map((preference, index) => {
-          return <Card key={index} value={preference.value} />;
-        })}
-      </div>
-
-      <div className={style.button}>
-        <Button
-          color="secondary"
-          variant="contained"
-          endIcon={<SendIcon style={{ fill: "#1A2D31" }} />}
-          onClick={handleClick}
+      <div className={`${style.preferencesSection}`}>
+        <Typography
+          sx={{ fontSize: "1.9rem", fontFamily: "Montserrat" }}
+          varient="h2"
+          component="h2"
         >
-          Get Started
-        </Button>
+          Select Preferences
+        </Typography>
+        <Typography sx={{ fontSize: ".9rem", fontFamily: "Montserrat", fontStyle: "italic", fontWeight: "100" }}
+                    varient="p"
+                    component="p">(At least 2, at most 5)</Typography>
+        <div className={style.preferences}>
+          {choices.map((preference, index) => {
+            return <Card key={index} value={preference.value} />;
+          })}
+        </div>
+
+        <div className={style.button}>
+          <Button
+            color="secondary"
+            variant="contained"
+            endIcon={<SendIcon style={{ fill: "#1A2D31" }} />}
+            onClick={handleClick}
+          >
+            Get Started
+          </Button>
+        </div>
       </div>
-    </div>
   );
 }
 
