f9ae8db394a367633da7854dd231d217d2bba25b
eddybruv <edwinajong@gmail.com>
2022-06-24 16:27:11 +0100

    conditionally render ContinueReading component

--- client/src/components/foryouPage/Hero.jsx
+++ client/src/components/foryouPage/Hero.jsx
@@ -1,17 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import style from "../../styles/hero.module.css";
 import SearchBar from "../SearchBar";
 import ContinueReading from "./ContinueReading";
 import Categories from "./Categories";
 import YourChoice from "./YourChoice";
+import axios from "axios"
 
 function Hero() {
+  const [books, setBooks] = useState(null);
+  const user = JSON.parse(sessionStorage.getItem("loggedUser"))
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.post("/api/book/get-books", {userID: user._id}).then(data => setBooks(data.data.data));
+    }
+    fetchData().then();
+  })
+
+
+
   return (
     <section className={`${style.body} container-fluid`}>
-      <SearchBar />
-      <ContinueReading/>
+      <SearchBar/>
+      {books?.length ? <ContinueReading/> : "" }
       <Categories/>
-      <YourChoice />
+      <YourChoice/>
     </section>
   );
 }
