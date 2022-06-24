6fa064b725c7ab93d1f808910a5a02cba51d22e6
eddybruv <edwinajong@gmail.com>
2022-06-24 15:24:33 +0100

    finished modal

--- client/src/components/foryouPage/BookModal.jsx
+++ client/src/components/foryouPage/BookModal.jsx
@@ -1,29 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
+import {IconButton, Button, ButtonGroup} from "@mui/material";
 
 import style from "../../styles/bookmodal.module.css"
+import OptionModal from "./OptionModal";
 const BookModal = ({closeModal, bookDetails}) => {
+
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleClose = (e) => {
+    closeModal();
+  }
+
+  const handleAdd = () => {
+    setShowOptions(true);
+  }
+
+  const handleCloseAdd = () => {
+    setShowOptions(!showOptions);
+  }
+
   return (
     <section className={style.body}>
-      <section className={style.photoSection}>
-        <img src={bookDetails.thumbnail} alt=""/>
+      <section className={style.content}>
+        <section className={style.photoSection}>
+          <img src={bookDetails.thumbnail} alt=""/>
+          <p className={style.mobileTitle}>{bookDetails.title}</p>
+        </section>
+        <section className={style.rightSection}>
+          <section className={style.titleBox}>
+            <p className={style.title}>{bookDetails.title}</p>
+          </section>
+          <section className={style.authorBox}>
+            <p className={style.author}>By {bookDetails.author}</p>
+          </section>
+          <section className={style.descBox}>
+            <p className={style.desc}>{bookDetails.description}</p>
+          </section>
+          <section className={style.pubBox}>
+            <p className={style.pubDate}>Publication Date: {bookDetails.publishedDate}</p>
+          </section>
+          <section className={style.pageBox}>
+            <p className={style.page}>{bookDetails.pageCount} Pages</p>
+          </section>
+          <section className={style.link}>
+            View on <a target={"_blank"} href={bookDetails.previewLink}>Google</a>
+          </section>
+          <ButtonGroup>
+            <Button color="secondary" onClick={handleClose}>Close</Button>
+            <Button color="secondary" variant="contained" onClick={handleAdd}>Add</Button>
+          </ButtonGroup>
+        </section>
       </section>
-      <section className={style.rightSection}>
-        <section className={style.titleBox}>
-          <p className={style.title}>Title: {bookDetails.title}</p>
-        </section>
-        <section className={style.authorBox}>
-          <p className={style.author}>Author: {bookDetails.author}</p>
-        </section>
-        <section className={style.pubBox}>
-          <p className={style.pubDate}>Publication Date: {bookDetails.publishedDate}</p>
-        </section>
-        <section className={style.descBox}>
-          <p className={style.desc}>Description: {bookDetails.description}</p>
-        </section>
-        <section className={style.pageBox}>
-          <p className={style.page}>Page Count: {bookDetails.pageCount}</p>
-        </section>
-      </section>
+      {showOptions &&
+        <OptionModal
+          closeOption={handleCloseAdd}
+          bookDetails={bookDetails}
+        />
+      }
     </section>
   )
 }
