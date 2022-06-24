1f330653bcbee662f6040c952d3726e6e8cb8dfb
eddybruv <edwinajong@gmail.com>
2022-06-24 15:23:55 +0100

    modified gotten books

--- client/src/components/foryouPage/ContinueReading.jsx
+++ client/src/components/foryouPage/ContinueReading.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from "../../styles/continue.module.css";
 import Card from './Card';
-import books from './book';
+import axios from "axios";
+// import books from './book';
 
 function ContinueReading() {
+  const user = JSON.parse(sessionStorage.getItem("loggedUser"));
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    const fetchServer = async () => {
+      const res = await axios.post("/api/book/get-books", {userID: user._id})
+        .then((data) => setBooks(data.data.data));
+    }
+    fetchServer().then();
+  }, [])
+
+  useEffect(() => {
+    console.log(books);
+  }, [books])
+
   return (
     <section className={style.previous}>
       <h2 className={style.previousText}>Continue Reading</h2>
       <section className={`row ${style.previousCard}`}>
-        {books.map((book, index) => {
+        { books !== null ?
+          books.map((book, index) => {
           return (
             <Card
               key={index}
-              title={book.volumeInfo.title}
-              author={book.volumeInfo.authors[0]}
-              publishedDate={book.volumeInfo.publishedDate}
-              description={book.volumeInfo.description}
-              pageCount={book.volumeInfo.pageCount}
-              smallThumbnail={book.volumeInfo.imageLinks.smallThumbnail}
-              thumbnail={book.volumeInfo.imageLinks.thumbnail}
-              previewLink={book.volumeInfo.previewLink}
+              title={book.title}
+              author={book.authors}
+              publishedDate={book.publishedDate}
+              description={book.description}
+              pageCount={book.pageCount}
+              smallThumbnail={book.smallThumbnail}
+              thumbnail={book.thumbnail}
+              previewLink={book.previewLink}
               user_id={index}
-              saleability={book.saleInfo.saleability}
-              averageRating={book.volumeInfo.averageRating}
+              // saleability={book.saleInfo.saleability}
+              // averageRating={book.averageRating}
             />
-          );
-        })}
+          )
+        }): "No books in your library"}
       </section>
     </section>
   );
